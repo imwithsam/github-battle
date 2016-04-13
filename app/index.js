a066b72ad207df603df3f31cc55dfef737777d59
Samson Brock <SamsonLB@gmail.com>
2016-04-12 22:56:00 -0700

    Create React Hello World example

--- app/index.js
+++ app/index.js
@@ -1,2 +1,15 @@
-var app = document.getElementById('app');
-app.innerHTML = 'Hello!';
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <div>Hello {this.props.name}!</div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <HelloWorld name="Sam" />,
+  document.getElementById('app')
+);
