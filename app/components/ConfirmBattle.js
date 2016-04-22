fe805936bd6384a04db5629623f6a79294e58285
Samson Brock <SamsonLB@gmail.com>
2016-04-22 10:08:28 -0700

    Refactor views into reusable components.

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -4,40 +4,38 @@ var styles = require('../styles')
 var Link = require('react-router').Link
 var UserDetails = require('../components/UserDetails')
 var UserDetailsWrapper = require('../components/UserDetailsWrapper')
-
-function puke (object) {
-  return <pre>{JSON.stringify(object, null, ' ')}</pre>
-}
+var MainContainer = require('../components/MainContainer')
 
 function ConfirmBattle (props) {
-  return props.isLoading
-    ? <p>LOADING!</p>
-    : <div className='jumbotron col-sm-122 text-center' style={styles.transparentBg}>
-        <h1>Confirm Players</h1>
-        <div className='col-sm-8 col-sm-offset-2'>
-          <UserDetailsWrapper header='Player One'>
-            <UserDetails info={props.playersInfo[0]} />
-          </UserDetailsWrapper>
-          <UserDetailsWrapper header='Player Two'>
-            <UserDetails info={props.playersInfo[1]} />
-          </UserDetailsWrapper>
+  if (props.isLoading) return <p>LOADING...</p>
+
+  return (
+    <MainContainer>
+      <h1>Confirm Players</h1>
+      <div className='col-sm-8 col-sm-offset-2'>
+        <UserDetailsWrapper header='Player One'>
+          <UserDetails info={props.playersInfo[0]} />
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header='Player Two'>
+          <UserDetails info={props.playersInfo[1]} />
+        </UserDetailsWrapper>
+      </div>
+      <div className='col-sm-8 col-sm-offset-2'>
+        <div className='col-sm-12' style={styles.space}>
+          <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
+            INITIATE BATTLE
+          </button>
         </div>
-        <div className='col-sm-8 col-sm-offset-2'>
-          <div className='col-sm-12' style={styles.space}>
-            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
-              INITIATE BATTLE
+        <div className='col-sm-12' style={styles.space}>
+          <Link to='playerOne'>
+            <button type='button' className='btn btn-lg btn-danger'>
+              Reselect Players
             </button>
-          </div>
-          <div className='col-sm-12' style={styles.space}>
-            <Link to='playerOne'>
-              <button type='button' className='btn btn-lg btn-danger'>
-                Reselect Players
-              </button>
-            </Link>
-          </div>
+          </Link>
         </div>
       </div>
-
+    </MainContainer>
+  )
 }
 
 ConfirmBattle.propTypes = {
---
+++ app/components/MainContainer.js
@@ -0,0 +1,13 @@
+// React required for JSX
+var React = require('react')
+var styles = require('../styles')
+
+function MainContainer (props) {
+  return (
+    <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
+      {props.children}
+    </div>
+  )
+}
+
+module.exports = MainContainer
