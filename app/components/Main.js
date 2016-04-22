a1c3f6bdce900ad4c04149ebcfcc37c25facb82f
Samson Brock <SamsonLB@gmail.com>
2016-04-22 11:30:16 -0700

    Add transition effect between pages.
    Uses react-addons-css-transition-group to create a page transition
    effect.
    
    style-loader and css-loader were added to Webpack so we can load css
    files in React JavaScript files.

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,10 +1,20 @@
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group')
+// We can require the css file below like it's a JS file thanks to the
+// style-loader and css-loader loaders in our Webpack config.
+require('../main.css')
 
 var Main = React.createClass({
   render: function () {
     return (
-      <div>
-        {this.props.children}
+      <div className='main-container'>
+        <ReactCSSTransitionGroup
+          transitionName='appear'
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}>
+          { /* ReactCSSTransitionGroup requires its child elements to have a key */ }
+          {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+        </ReactCSSTransitionGroup>
       </div>
     )
   }
